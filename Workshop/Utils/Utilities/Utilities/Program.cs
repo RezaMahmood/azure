356fb1e8054935308481c634994952920adcaea3
Reza Mahmood <remah@microsoft.com>
2019-03-13 18:04:09 +0000

    added utility to get JWT token

---
+++ Workshop/Utils/Utilities/Utilities/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var authContext = new AuthenticationContext("https://login.microsoftonline.com/" + args[0]);
+            var result = authContext.AcquireTokenAsync("https://management.azure.com", args[1], new Uri(""+ args[2] +""), new PlatformParameters(PromptBehavior.Auto)).Result;
+
+            if (result == null)
+            {
+                throw new InvalidOperationException("Failed to obtain JWT token");
+            }
+
+            Console.WriteLine(result.AccessToken);
+
+            Console.ReadLine();
+
+        }
+    }
+}
